cca2afe96d753e7ed07c62028087aac246ed6e6d
diefans <oliver@digitalarchitekt.de>
2015-04-24 20:30:55 +0200

    first running setup

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM ansible/ubuntu14.04-ansible:devel
 
+RUN apt-get update -yqq
+RUN apt-get upgrade -yqq
+RUN apt-get install vim -yqq
+
 # smtp
 EXPOSE 25
 EXPOSE 587
@@ -16,17 +20,33 @@ EXPOSE 993
 ADD ansible /srv/postamt/
 WORKDIR /srv/postamt
 
+ENV PATH $PATH:/srv/postamt/bin:/usr/bin:/bin:/usr/local/bin:/sbin:/usr/local/sbin:/usr/sbin
+ENV POSTAMT_ROOT /srv/postamt
+ENV POSTAMT_DATA $POSTAMT_ROOT/data
+ENV POSTAMT_VMAIL $POSTAMT_DATA/vmail
+ENV POSTAMT_DB $POSTAMT_DATA/mail.sqlite
+
+ENV HOSTNAME localhost
+
+# persist ssl
+#ENV POSTAMT_SSL_MAKE 0
+ENV POSTAMT_SSL $POSTAMT_DATA/ssl
+ENV POSTAMT_SSL_CN example.com
+ENV POSTAMT_SSL_C DE
+ENV POSTAMT_SSL_ST Berlin
+ENV POSTAMT_SSL_L Berlin
+#ENV POSTAMT_SSL_O foobar
+
+ENV POSTAMT_DOVECOT_SSL $POSTAMT_SSL/dovecot
+ENV POSTAMT_DOVECOT_SSL_CN $POSTAMT_SSL_CN
+
+ENV POSTAMT_POSTFIX_SSL $POSTAMT_SSL/postfix
+ENV POSTAMT_POSTFIX_SSL_CN $POSTAMT_SSL_CN
+
+
 # Run Ansible to configure the Docker image
 RUN ansible-playbook site.yml -c local -vv
 
-ENV PATH /srv/postamt/bin:/usr/bin:/bin:/usr/local/bin:/sbin:/usr/local/sbin:/usr/sbin
-
-ENV DB /srv/postamt/data/mail.sqlite
-
-ADD create.sql /srv/postamt/create.sql
 ADD start-postamt.sh /srv/postamt/bin/start-postamt.sh
-
-ADD admin.sh /srv/postamt/bin/admin.sh
-ADD admin.py /srv/postamt/bin/admin.py
 
 ENTRYPOINT ["start-postamt.sh"]
--- start-postamt.sh
+++ start-postamt.sh
@@ -1,14 +1,62 @@
 #!/bin/bash
 
-# first check for db
-if [ ! -f "$DB" ]; then
-    echo "creating db: $DB" >&2
-    # create db
-    cat create.sql | sqlite3 -echo $DB;
-else
-    echo "using db: $DB" >&2
+# initialize database
+postamt init
 
+if [ ! -f $POSTAMT_VMAIL ]; then
+    mkdir -p $POSTAMT_VMAIL;
+    chown postamt.postamt $POSTAMT_VMAIL;
 fi
+
+# to have a mounted data volume functional
+# we have to setup resp. ensure some things to be there
+
+function create_self_signed_cert {
+    BASE=$1
+    CN=$2
+
+    openssl req -new -x509 -nodes\
+        -days 3650\
+        -subj "/C=$POSTAMT_SSL_C/ST=$POSTAMT_SSL_ST/L=$POSTAMT_SSL_L/CN=$CN"\
+        -newkey rsa:2048\
+        -keyout $BASE.key -out $BASE.crt
+
+    openssl x509 -subject -fingerprint -noout -in $BASE.crt || exit 2
+
+    chgrp postamt $KEY
+    chmod 0660 $KEY
+}
+
+mkdir -p $POSTAMT_SSL
+
+# setup hostname
+echo $HOSTNAME > /etc/hostname
+
+# dovecot ssl
+if [ ! -f "$POSTAMT_DOVECOT_SSL.key" ] || [ ! -z "$POSTAMT_SSL_MAKE" ]; then
+    # create new
+    create_self_signed_cert $POSTAMT_DOVECOT_SSL $POSTAMT_DOVECOT_SSL_CN
+fi
+
+# postfix ssl
+if [ ! -f $POSTAMT_POSTFIX_SSL.key ] || [ ! -z "$POSTAMT_SSL_MAKE" ]; then
+    # create new
+    create_self_signed_cert $POSTAMT_POSTFIX_SSL $POSTAMT_POSTFIX_SSL_CN
+fi
+
+ls -lisa --color=auto $POSTAMT_SSL
+
+# postfix helo_checks
+touch $POSTAMT_DATA/helo_checks
+
+
+# TODO may be turn this off
+# aliases
+touch /var/lib/mailman/aliases
+/usr/lib/mailman/bin/genaliases
+newaliases
+
+
 
 # start
 supervisord -n
